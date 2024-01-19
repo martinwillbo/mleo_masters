84bc8b0e8021a2a7085fce4b7fb9f0e39cdfce54
Agnes Eriksson <3erikssonagnes@gmail.com>
2024-01-19 16:37:35 +0100

    stats_time

--- stats.py
+++ stats.py
@@ -16,21 +16,20 @@ def stats(config):
     train_iter = iter(train_loader)
 
     for X, _ in tqdm(train_iter):
-        batch_size = X.size(0)
         num_batches += 1
         X = X[:, :3, :, :]
         # Calculate channel-wise sums and squared sums
         channel_sums += X.sum(dim=(0, 2, 3))
         print(channel_sums)
     # Calculate mean and standard deviation across all batches
-    mean = channel_sums / (num_batches * batch_size * 512 * 512)
+    mean = channel_sums / (num_batches * config.batch_size * 512 * 512)
     print(mean)
 
     train_iter = iter(train_loader)
     for X, _ in tqdm(train_iter):
         X = X[:, :3, :, :]
-        X_reshaped = X.view(1, 3, 1, 1)
-        X_diff = X_reshaped - channel_sums
+        channel_reshaped = channel_sums.view(1, 3, 1, 1)
+        X_diff = X - channel_reshaped
         channel_squared_diff += (X_diff**2).sum(dim=(0, 2, 3))
     variance = (channel_squared_diff / (batch_size * num_batches * 512 * 512))
     std = torch.sqrt(variance)
