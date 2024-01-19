ca9b09235a043c1104ba98e0c11fd1f178e2ecab
Agnes Eriksson <3erikssonagnes@gmail.com>
2024-01-19 16:09:35 +0100

    stats_time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import random
 import loop
+import stats
 from torch.utils.tensorboard import SummaryWriter
 
 @hydra.main(config_path='config', config_name='config', version_base = '1.3.2')
@@ -15,8 +16,11 @@ def main(config):
 
     #NOTE: Don't have to use tensorboard to log experiments, but should implement something else if so (Aleksis have code for this).
     #name more cleverly
-    writer = SummaryWriter(log_dir='/raid/dlgroupmsc/writer_${now:%Y-%m-%d_%H-%M-%S}')
-    loop.loop(config, writer)
+    mean, std = stats(config)
+    print(mean)
+    print(std)
+    #writer = SummaryWriter(log_dir='/raid/dlgroupmsc/writer_${now:%Y-%m-%d_%H-%M-%S}')
+    #loop.loop(config, writer)
 
 if __name__ == '__main__':
     main()
---
+++ stats.py
@@ -0,0 +1,28 @@
+from torch.utils.data import DataLoader
+import util
+def stats(config):
+    dataset_module = util.load_module(config.dataset.script_location)
+    #NOTE: Just outlining the 'interface' of this way of structuring the experiment code
+    train_set = dataset_module.train_set(config)
+    train_loader = DataLoader(train_set, batch_size = config.train_batch_size, shuffle = False, num_workers = config.num_workers,
+                            pin_memory = True)
+    # Initialize variables for accumulating channel-wise sums
+    channel_sums = torch.zeros(3)
+    channel_squared_diff = torch.zeros(3)
+    for X, _ in train_loader:
+        batch_size = X.size(0)
+        num_batches += 1
+        X = X(:, :3, :, :)
+        # Calculate channel-wise sums and squared sums
+        channel_sums += X.sum(dim=(0, 2, 3))
+    # Calculate mean and standard deviation across all batches
+    mean = channel_sums / (num_batches * batch_size * 512 * 512)
+    for X, _ in train_loader:
+        X = X(:, :3, :, :)
+        X_diff = X - channel_sums
+        channel_squared_diff += (X_diff**2).sum(dim=(0, 2, 3))
+    variance = (channel_squared_diff / (batch_size * num_batches * 512 * 512))
+    std = torch.sqrt(variance)
+    print(f"Mean across channels: {mean}")
+    print(f"Standard deviation across channels: {std}")
+    return mean, std
