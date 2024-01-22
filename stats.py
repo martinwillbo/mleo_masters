81a06b7e822a1d5e5edb0456c931c800e4e250b6
Agnes Eriksson <3erikssonagnes@gmail.com>
2024-01-22 12:14:26 +0100

    test

--- stats.py
+++ stats.py
@@ -12,7 +12,7 @@ def stats(config):
     # Initialize variables for accumulating channel-wise sums
     channel_sums = torch.zeros(5)
     channel_squared_diff = torch.zeros(5)
-    num_batches = 0
+    #num_batches = 0
     train_iter = iter(train_loader)
     num_img = 0
 
@@ -24,7 +24,7 @@ def stats(config):
         # Calculate channel-wise sums and squared sums
         channel_sums += X.sum(dim=(0, 2, 3))
     # Calculate mean and standard deviation across all batches
-    mean = channel_sums / (num_batches * num_img * 512 * 512)
+    mean = channel_sums / (num_img * 512 * 512)
     print(mean)
 
     train_iter = iter(train_loader)
@@ -33,12 +33,11 @@ def stats(config):
         X = X[:, :, :, :]
         print(X.mean(dim=(0,2,3)))
         channel_reshaped = mean.view(1, 5, 1, 1)
-        print(channel_reshaped)
         X_diff = X - channel_reshaped
         print(X_diff.mean(dim=(0,2,3)))
         channel_squared_diff += (X_diff**2).sum(dim=(0, 2, 3))
     
-    variance = (channel_squared_diff / (config.batch_size * num_batches * 512 * 512))
+    variance = (channel_squared_diff / (num_img * 512 * 512))
     std = torch.sqrt(variance)
     print(f"Mean across channels: {mean}")
     print(f"Standard deviation across channels: {std}")
