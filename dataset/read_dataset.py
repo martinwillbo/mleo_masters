0c99491d15f4ef3c0a36333ca1e9f883fe97a532
malteahman <malte.ahman@gmail.com>
2024-01-23 15:32:37 +0100

    setup test

--- dataset/read_dataset.py
+++ dataset/read_dataset.py
@@ -82,15 +82,9 @@ class DatasetClass(Dataset):
         if self.config.dataset.scale:
             x = self._rescale(x, is_label = False)
             y = self._rescale(y, is_label = True)
-        if self.config.use_transform:
-            
-            if index < 5:
-                print('Not transformed')
-                print(x)
+        if self.config.use_transform:            
             x, y = self.transform.apply(x,y)
-            if index < 5:
-                print('Transformed')
-                print(x)
+           
         #NOTE: These operations expect shape (H,W,C)
         #Normalize images, after transform
         x = np.transpose(x, (1,2,0)).astype(float)
@@ -98,9 +92,7 @@ class DatasetClass(Dataset):
         x /= self.layer_stds
         #NOTE: Pytorch models typically expect shape (C, H, W)
         x = np.transpose(x, (2,0,1))
-        if index < 5:
-            print('Normalized')
-            print(x)
+        
         return torch.tensor(x, dtype = torch.float), torch.tensor(y, dtype = torch.long)
     def __len__(self):
         assert len(self.X_tif_paths) == len(self.Y_tif_paths)
