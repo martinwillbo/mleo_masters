88862385f4f5c89fa677e140fa09780c6ab2fe46
malteahman <malte.ahman@gmail.com>
2024-01-23 14:14:11 +0100

    setup test

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main(config):
     #NOTE: Don't have to use tensorboard to log experiments, but should implement something else if so (Aleksis have code for this).
     #name more cleverly
     current_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    hydra_log_dir = '/../log_res/'+current_timestamp
+    hydra_log_dir = '../log_res/'+current_timestamp
     log_dir = os.path.join(hydra_log_dir, 'tensorboard')
     print(log_dir)
     writer = SummaryWriter(log_dir=log_dir)
