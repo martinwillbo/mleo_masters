510afee81851ea878e957276714472de2692438d
Agnes Eriksson <3erikssonagnes@gmail.com>
2024-01-23 11:20:13 +0100

    get correct folders

--- loop.py
+++ loop.py
@@ -92,7 +92,8 @@ def loop(config, writer = None):
 
             #optimizer.step()
             #NOTE: If you have a learning rate scheduler this is to place to step it. 
-            y_pred = torch.argmax(y_pred, dim=1) #sets class to each dp
+            y_pred = torch.argmax(y_pred, dim=1) #sets class to each data point
+            print(y_pred.shape)
             y_pred = y_pred.cpu().contiguous()
             y = y.cpu().contiguous()
             y_pred_flat = y_pred.view(-1).numpy() 
