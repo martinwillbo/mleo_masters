8871ec39da2305de0c68106b30953abd1c37de6e
Agnes Eriksson <3erikssonagnes@gmail.com>
2024-01-23 11:10:29 +0100

    get correct folders

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import loop
 import stats
 from torch.utils.tensorboard import SummaryWriter
 import os
+from datetime import datetime
 
 @hydra.main(config_path='config', config_name='config', version_base = '1.3.2')
 def main(config):
@@ -16,7 +17,8 @@ def main(config):
     torch.cuda.manual_seed(config.seed)
 
     #NOTE: Don't have to use tensorboard to log experiments, but should implement something else if so (Aleksis have code for this).
-    hydra_log_dir = '/raid/dlgroupmsc/${now:%Y-%m-%d_%H-%M-%S}'
+    current_timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    hydra_log_dir = '/raid/dlgroupmsc/'+current_timestamp
     log_dir = os.path.join(hydra_log_dir, 'tensorboard')
     print(log_dir)
     writer = SummaryWriter(log_dir=log_dir)
