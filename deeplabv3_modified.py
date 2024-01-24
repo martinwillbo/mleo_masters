54ff68615465a613f582169ed238d13788a5be3d
malteahman <malte.ahman@gmail.com>
2024-01-24 17:10:21 +0100

    main test

--- deeplabv3_modified.py
+++ deeplabv3_modified.py
@@ -16,8 +16,10 @@ class DeepLabV3Modified(nn.Module):
         # Modify the input channels of the first convolution layer
         self.resnet50.backbone.conv1 = nn.Conv2d(num_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
+        aspp_in_channels = self.resnet50.classifier[0].in_channels # 256*4 acc to chatgpt
+
         # Change the number of input channels for the ASPP module
-        self.resnet50.classifier[0] = nn.Conv2d(256 * 4, num_classes, kernel_size=(1, 1), stride=(1, 1)) #was 256*4 from the begininin
+        self.resnet50.classifier[0] = nn.Conv2d(aspp_in_channels, num_classes, kernel_size=(1, 1), stride=(1, 1)) #was 256*4 from the begininin
 
     def forward(self, x):
         return self.resnet50(x)
