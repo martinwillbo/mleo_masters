88f1e378db2fdb1c7ba415bbe265ef81adee5f66
malteahman <malte.ahman@gmail.com>
2024-01-24 17:05:45 +0100

    main test

--- deeplabv3_modified.py
+++ deeplabv3_modified.py
@@ -17,7 +17,7 @@ class DeepLabV3Modified(nn.Module):
         self.resnet50.backbone.conv1 = nn.Conv2d(num_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
         # Change the number of input channels for the ASPP module
-        self.resnet50.classifier[0] = nn.Conv2d(256 * 5, num_classes, kernel_size=(1, 1), stride=(1, 1)) #was 256*4 from the begininin
+        self.resnet50.classifier[0] = nn.Conv2d(256 * 4, num_classes, kernel_size=(1, 1), stride=(1, 1)) #was 256*4 from the begininin
 
     def forward(self, x):
         return self.resnet50(x)
