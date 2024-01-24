2d24c923b8807521ee85149d7d170a23e4c65dca
malteahman <malte.ahman@gmail.com>
2024-01-24 17:01:41 +0100

    main test

--- deeplabv3_modified.py
+++ deeplabv3_modified.py
@@ -11,13 +11,13 @@ class DeepLabV3Modified(nn.Module):
         super(DeepLabV3Modified, self).__init__()
 
         # Load the pre-trained DeepLabV3 model with ResNet-50 backbone
-        self.resnet50 = models.segmentation.deeplabv3_resnet50(pretrained=True)
+        self.resnet50 = models.segmentation.deeplabv3_resnet50(weights = config.model.pretrained, weights_backbone = config.model.pretrained_backbone)
 
         # Modify the input channels of the first convolution layer
         self.resnet50.backbone.conv1 = nn.Conv2d(num_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
         # Change the number of input channels for the ASPP module
-        self.resnet50.classifier[0] = nn.Conv2d(256 * 4, num_classes, kernel_size=(1, 1), stride=(1, 1))
+        self.resnet50.classifier[0] = nn.Conv2d(256 * 5, num_classes, kernel_size=(1, 1), stride=(1, 1)) #was 256*4 from the begininin
 
     def forward(self, x):
         return self.resnet50(x)
