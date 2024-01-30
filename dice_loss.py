4641fc29958cd25c48460b32f802ec9f6a2f18e9
malteahman <malte.ahman@gmail.com>
2024-01-30 15:22:09 +0100

    image save

--- dice_loss.py
+++ dice_loss.py
@@ -12,7 +12,7 @@ class DiceLoss(nn.Module):
 
     def forward(self, y_pred, y):
 
-        #y_pred = torch.argmax(y_pred, dim=1) # this is already done outside the loss 
+        y_pred = torch.argmax(y_pred, dim=1) # this is not done outside the loss 
 
         dice_coeffs = torch.zeros((self.num_classes,), dtype=torch.float).to(self.config.device)  #creates empty vecn
         
