9fc5aecf1c04661cc70c52a8ba6e64d9b6d3bee1
malteahman <malte.ahman@gmail.com>
2024-01-30 10:49:59 +0100

    image save

---
+++ dice_loss.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DiceLoss(nn.Module):
+    def __init__(self, num_classes, epsilon=1e-5):
+        super(DiceLoss, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+
+    def forward(self, y_pred, y):
+
+        dice = torch.zeros((self.num_classes,), dtype=torch.float)  #creates empty vecn
+        
+        y_pred_flat = y_pred.view(-1)
+        y_flat = y.view(-1)
+
+        for i in range(self.num_classes): #for all classes
+
+            y_flat_i = y_flat == i #sets ones where y_flat is equal to i
+            pred_flat_i = y_pred_flat == i
+            intersection_i = torch.logical_and(y_flat_i, pred_flat_i) #where they match
+            union_i = torch.logical_or(y_flat_i, pred_flat_i) #everything together
+            num_intersection_i = torch.count_nonzero(intersection_i).item() #how big is the intersection
+            num_union_i = torch.count_nonzero(union_i).item() #how big is the union
+
+            dice[i] = 1 - (2 * num_intersection_i)/(num_union_i + self.epsilon)
+
+        dice_loss = torch.mean(dice)
+        return dice_loss
