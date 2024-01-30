14dc834598142f4116d842f8b03d4cfb8c27ebbf
malteahman <malte.ahman@gmail.com>
2024-01-30 15:30:05 +0100

    image save

--- loop3.py
+++ loop3.py
@@ -302,7 +302,7 @@ def loop3(config, writer, hydra_log_dir):
             #y_pred = torch.argmax(y_pred, dim=1) #sets class to each data point
                 #y_pred = model(x)
             l = train_loss(y_pred, y)
-            
+            y_pred = torch.argmax(y_pred, dim=1)
             l.backward()
             optimizer.step()
             #scaler.scale(l).backward()
