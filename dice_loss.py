9304190f57a5c31df6d651be5bc8e3a2779ab66a
malteahman <malte.ahman@gmail.com>
2024-01-31 13:55:00 +0100

    image save

--- dice_loss.py
+++ dice_loss.py
@@ -12,8 +12,7 @@ class DiceLoss(nn.Module):
 
     def forward(self, y_pred, y):
 
-        print(y.shape)
-        print(y_pred.shape)
+    
 
         y = y.to(torch.float32)
         y_pred = y_pred.to(torch.float32)
@@ -35,9 +34,10 @@ class DiceLoss(nn.Module):
 
             dice_coeffs[i] = (2 * num_intersection_i + self.epsilon)/(num_y_i + num_y_pred_i + self.epsilon)
 
-        print(dice_coeffs)
+        
         dice_loss = torch.tensor(1.0) - torch.mean(dice_coeffs)
         dice_loss.requires_grad_ = True
+        print(dice_loss.dtype)
         #dice_loss = dice_loss.to(device = self.config.device, dtype= torch.float32)       
         
         return dice_loss 
