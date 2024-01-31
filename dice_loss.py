b6ec376adf2423c11476d3d6f92d2b7c33b4eb68
malteahman <malte.ahman@gmail.com>
2024-01-31 10:41:15 +0100

    image save

--- dice_loss.py
+++ dice_loss.py
@@ -35,6 +35,6 @@ class DiceLoss(nn.Module):
         dice_loss = torch.tensor(1.0) - torch.mean(dice_coeffs)
         #dice_loss = 1 -torch.mean(dice_coeffs)
         dice_loss = torch.tensor(dice_loss, requires_grad=True) 
-        dice_loss = dice_loss.to(dtype= torch.float32)
+        dice_loss = dice_loss.to(device = self.config.device, dtype= torch.float32)
         #print(dice_loss.dtype)
         return dice_loss 
