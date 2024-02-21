380316698d914c1347cad6286d83fefa961daf23
malteahman <malte.ahman@gmail.com>
2024-02-21 12:00:53 +0100

    sentinel data

--- stats.py
+++ stats.py
@@ -19,7 +19,7 @@ def stats(config):
     for _, senti, _ in tqdm(train_iter):
         #dim will be batch x month x channel x height x
         num_img += len(senti)
-        senti = senti.mean(senti, dim=1)
+        senti = torch.mean(senti, dim=1)
         #print(senti.mean(dim=(0,2,3)))
         # Calculate channel-wise sums and squared sums
         channel_sums += senti.sum(dim=(0, 2, 3))
@@ -30,11 +30,11 @@ def stats(config):
     train_iter = iter(train_loader)
     for _, senti, _ in tqdm(train_iter):
         #DOES NOT WORK
-        senti = senti.mean(senti, dim=1)
+        senti = torch.mean(senti, dim=1)
         #print(X.mean(dim=(0,2,3)))
         channel_reshaped = mean.view(1, 5, 1, 1)
         _diff = senti - channel_reshaped
-        print(_diff.mean(dim=(0,2,3)))
+        print(torch.mean(_diff, dim=(0,2,3)))
         channel_squared_diff += (_diff**2).sum(dim=(0, 2, 3))
     
     variance = (channel_squared_diff / (num_img * 21 * 21))
