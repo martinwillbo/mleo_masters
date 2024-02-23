2d3520018956ddb515dc774a910e618c8241cf69
malteahman <malte.ahman@gmail.com>
2024-02-23 11:27:34 +0100

    sentinel data

--- loop3.py
+++ loop3.py
@@ -9,7 +9,7 @@ from torch.optim import Adam, SGD
 import sys
 from torch.cuda.amp import autocast, GradScaler
 import segmentation_models_pytorch as smp
-from unet_module import UnetFeatureSenti
+from unet_module import UnetFeatureSenti, UnetSentiUnet
 import os
 import math
 import random
@@ -47,7 +47,7 @@ def loop3(config, writer, hydra_log_dir):
         model.backbone.conv1 = nn.Conv2d(config.model.n_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
     if config.model.name == 'unet_senti':
-        model = UnetFeatureSenti(n_channels=config.model.n_channels, n_senti_channels=120, n_classes=config.model.n_class)
+        model = UnetSentiUnet(n_channels=config.model.n_channels, n_senti_channels=120, n_classes=config.model.n_class)
     #model = FCN8s(n_class=config.model.n_class, dim_input=config.model.n_channels, weight_init='normal')
 
     model = model.to(config.device)
