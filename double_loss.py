dca3c6d14af2691b7e89d7e6d01d25a79f0d2965
malteahman <malte.ahman@gmail.com>
2024-02-27 10:41:38 +0100

    sentinel data

--- double_loss.py
+++ double_loss.py
@@ -2,7 +2,7 @@ import segmentation_models_pytorch as smp
 import torch
 import torch.nn as nn
 class senti_loss(nn.Module):
-    def __init__(self, senti_weight=0.15):
+    def __init__(self, senti_weight=0.25):
         super(senti_loss, self).__init__()
         self.senti_loss = smp.losses.TverskyLoss(mode='multiclass', ignore_index=11) #ignore the 11th index of plowed land
         self.unet_loss = smp.losses.TverskyLoss(mode='multiclass')
