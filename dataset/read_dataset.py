4e767937dcd3ae71737050910aaa0d1a2d3a46c5
malteahman <malte.ahman@gmail.com>
2024-03-05 15:54:29 +0100

    sentinel data

--- dataset/read_dataset.py
+++ dataset/read_dataset.py
@@ -173,8 +173,9 @@ class DatasetClass(Dataset):
             senti = self._normalize_senti(senti)
             #senti = senti[:,:-2,:,:]
             monthly_senti = self._monthly_image(senti, dates)
+            return torch.tensor(x, dtype = torch.float), torch.tensor(y, dtype = torch.long), torch.tensor(monthly_senti, dtype = torch.float)
         
-        return torch.tensor(x, dtype = torch.float), torch.tensor(y, dtype = torch.long), torch.tensor(monthly_senti, dtype = torch.float)
+        return torch.tensor(x, dtype = torch.float), torch.tensor(y, dtype = torch.long)
     
     def __len__(self):
         assert len(self.X_tif_paths) == len(self.Y_tif_paths)
